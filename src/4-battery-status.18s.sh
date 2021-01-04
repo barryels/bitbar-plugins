4eaab7407f2f5648833318650b4f8dc44805cc26
Barry Els <barryels@gmail.com>
2021-01-04 14:41:43 +0200

    simplify battery status display

--- src/4-battery-status.18s.sh
+++ src/4-battery-status.18s.sh
@@ -14,71 +14,58 @@
 # Directly view battery time remaining:
 # $ echo $(pmset -g batt | egrep "([0-9]+\%).*" -o) | grep -oE '[0-9]+\:[0-9]{2}'
 
-MIN_BATTERY_LEVEL=40
-MAX_BATTERY_LEVEL=80
 
-COLOUR_RED='\033[0;31m'
-COLOUR_GREEN='\033[0;32m'
-COLOUR_LIGHT_GRAY='\033[0;37m'
-
-STYLE_CLEAR='\033[0m'
-STYLE_DEFAULT_TEXT=$COLOUR_LIGHT_GRAY
-
-
-BATTERY_DISCHARGING_SEARCH_TERM="discharging"
-
+### <DataGathering> ###
 
 battery_info=$(pmset -g batt | egrep "([0-9]+\%).*" -o)
 more_info=$(system_profiler SPPowerDataType)
-
 battery_level=$(echo $battery_info | cut -f1 -d';' | grep -o '[0-9]\+')
 battery_remaining_time=$(echo $battery_info | grep -oE '[0-9]+\:[0-9]{2}')
-battery_remaining_time="($battery_remaining_time)"
-is_battery_charging="yes"
 cycle_count=$(echo "$more_info" | grep "Cycle Count" | awk '{print $3}')
 wattage=$(echo "$more_info" | grep "Wattage (W)" | awk '{print $3}')
+is_battery_charging="yes"
+if echo "$battery_info" | grep -q "discharging"; then is_battery_charging="no"; fi
 
-if echo "$battery_info" | grep -q "$BATTERY_DISCHARGING_SEARCH_TERM";
-then
-    is_battery_charging="no";
-fi
+### </DataGathering> ###
+
 
 
 ### <Testing> ###
 
 # battery_level=39
 # battery_remaining_time=""
+# battery_remaining_time="1:30"
+
 # is_battery_charging="no"
 # is_battery_charging="yes"
-
-# icon_lightning_bolt="⚡"
-# icon_check="✓"
-# icon_up="↑"
-# icon_down="↓"
 
 ### </Testing> ###
 
 
-if [ "$battery_remaining_time" = "()" ]; then battery_remaining_time="..."; fi
-if [ "$battery_remaining_time" = "(0:00)" ]; then battery_remaining_time=""; fi
 
-if [ "$is_battery_charging" = "yes" ];
-	then
-		status_icon="↑";
-		if [ "$battery_level" -ge 100 ];
-			then
-				status_icon="✓";
-		fi
-	else
-		status_icon="↓";
-fi
+### <DisplayFormatting> ###
+
+COLOUR_RED='\033[0;31m'
+COLOUR_GREEN='\033[0;32m'
+COLOUR_LIGHT_GRAY='\033[0;37m'
+STYLE_CLEAR='\033[0m'
+STYLE_DEFAULT_TEXT=$COLOUR_LIGHT_GRAY
 
 
-prefix="${COLOUR_GREEN}$battery_level$suffix"
-suffix="${STYLE_CLEAR}${STYLE_DEFAULT_TEXT} $status_icon $battery_remaining_time${STYLE_CLEAR} | size=12"
+if [ "$battery_remaining_time" = "" ]; then battery_remaining_time="..."; fi
+if [ "$battery_remaining_time" = "0:00" ]; then battery_remaining_time=""; fi
+if [ "$battery_remaining_time" != "" ]; then battery_remaining_time_text=" ($battery_remaining_time)"; fi
+suffix="${STYLE_CLEAR}${STYLE_DEFAULT_TEXT}$battery_remaining_time_text${STYLE_CLEAR} | size=12"
 
-if [ "$battery_level" -lt "$MIN_BATTERY_LEVEL" ]; then prefix="${COLOUR_RED}$battery_level$suffix"; fi
-if [ "$battery_level" -eq 100 ]; then prefix="${STYLE_CLEAR}${STYLE_DEFAULT_TEXT}$battery_level$suffix"; fi
+
+if [ "$is_battery_charging" = "yes" ]; then prefix="${COLOUR_GREEN}$battery_level"; fi
+if [ "$battery_level" -ge 100 ]; then prefix="${STYLE_CLEAR}${STYLE_DEFAULT_TEXT}$battery_level"; fi
+if [ "$is_battery_charging" = "no" ]; then prefix="${COLOUR_RED}$battery_level"; fi
+
+### </DisplayFormatting> ###
+
+
+### <Output> ###
 
 echo -e "${prefix}${suffix}"
 
@@ -86,3 +73,5 @@ echo "---"
 
 echo "Cycle Count: ${cycle_count}"
 echo "Wattage: ${wattage}"
+
+### </Output> ###
